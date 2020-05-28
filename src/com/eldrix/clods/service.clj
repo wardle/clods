a840b19281ac02905bf82cd2e937de5d2aebf53a
Mark Wardle <mark@wardle.org>
2020-05-28 20:38:49 +0100

    Remove unused code/comments

--- src/com/eldrix/clods/service.clj
+++ src/com/eldrix/clods/service.clj
@@ -8,10 +8,9 @@
     [ring.middleware.params :as params]
     [next.jdbc :as jdbc]))
 
-
+; TODO: add connection pooling
 (def db {:dbtype "postgresql" :dbname "ods"})
 (def ds (jdbc/get-datasource db))
-
 
 (defroutes app-routes
            (context "/v1" []
@@ -32,25 +31,10 @@
 (def app
   (wrap-defaults app-routes site-defaults))
 
-
-(defn handler [request]
-  (println request)
-  {:status  200
-   :headers {"Content-Type" "application/json"}
-   :body    (:org (first (jdbc/execute! ds ["SELECT data::varchar as org FROM organisations WHERE id = ?"
-                                            "2.16.840.1.113883.2.1.3.2.4.18.48|X26"])))})
-
-
-
-
 (defn start []
   (jetty/run-jetty #'app {:port 3000, :join? false})
   (println "server running in port 3000"))
 
 (comment
   (start)
-
-  (into [] (map str)
-        (jdbc/plan ds ["SELECT data#>>'{}' as org FROM organisations WHERE id = ?"
-                       "2.16.840.1.113883.2.1.3.2.4.18.48|X26"]))
   )
