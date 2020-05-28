62fdeec05cf957cf13ce8272439b771550c9795d
Mark Wardle <mark@wardle.org>
2020-05-28 20:49:09 +0100

    Add simple resolve identifier endpoint.
    This is for FHIR resolution, but we simply map to our master OID and then resolve.

--- src/com/eldrix/clods/service.clj
+++ src/com/eldrix/clods/service.clj
@@ -12,20 +12,29 @@
 (def db {:dbtype "postgresql" :dbname "ods"})
 (def ds (jdbc/get-datasource db))
 
+
+(def system->oid {
+                "https://fhir.nhs.uk/Id/ods-organization-code" "2.16.840.1.113883.2.1.3.2.4.18.48"
+                  "https://fhir.nhs.uk/Id/ods-site-code" "2.16.840.1.113883.2.1.3.2.4.18.48"
+                })
+
+(defn get-by-oid [root id]
+  (if-let [org (:org (first (jdbc/execute! ds ["SELECT data::varchar as org FROM organisations WHERE id = ?"
+                                               (str root "|" id)])))]
+    {:status  200
+     :headers {"Content-Type" "application/json"}
+     :body    (:org (jdbc/execute-one! ds ["SELECT data::varchar as org FROM organisations WHERE id = ?"
+                                           (str root "|" id)]))}
+    {:status 404}))
+
+
 (defroutes app-routes
-           (context "/v1" []
-             (context "/oid" []
-             (GET "/:root/:id" [root id]
-               (if-let [org (:org (first (jdbc/execute! ds ["SELECT data::varchar as org FROM organisations WHERE id = ?"
-                                                            (str root "|" id)])))]
-                 {:status  200
-                  :headers {"Content-Type" "application/json"}
-                  :body    (:org (jdbc/execute-one! ds ["SELECT data::varchar as org FROM organisations WHERE id = ?"
-                                                           (str root "|" id)]))}
-                 {:status 404})))
-             (GET "/search" []
-               {:status 500
-                :body "Not implemented"}))
+           (GET "/v1/oid/:root/:id" [root id] (get-by-oid root id))
+           (GET "/v1/resolve/:id" [id system]
+             (get-by-oid (get system->oid system) id))
+           (GET "/v1/oid/search" []
+             {:status 500
+              :body   "Not implemented"})
            (route/not-found "Not Found"))
 
 (def app
