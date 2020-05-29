97cf230b7565020b9297b584684d70906728f820
Mark Wardle <mark@wardle.org>
2020-05-29 11:30:06 +0100

    Add postcode endpoint.

--- src/com/eldrix/clods/service.clj
+++ src/com/eldrix/clods/service.clj
@@ -6,7 +6,8 @@
     [compojure.route :as route]
     [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
     [ring.middleware.params :as params]
-    [next.jdbc :as jdbc]))
+    [next.jdbc :as jdbc]
+    [com.eldrix.clods.postcode :as postcode]))
 
 ; TODO: add connection pooling and runtime configuration
 (def db {:dbtype "postgresql" :dbname "ods"})
@@ -14,23 +15,31 @@
 
 
 (def system->oid {
-                "https://fhir.nhs.uk/Id/ods-organization-code" "2.16.840.1.113883.2.1.3.2.4.18.48"
-                  "https://fhir.nhs.uk/Id/ods-site-code" "2.16.840.1.113883.2.1.3.2.4.18.48"
-                })
+                  "https://fhir.nhs.uk/Id/ods-organization-code" "2.16.840.1.113883.2.1.3.2.4.18.48"
+                  "https://fhir.nhs.uk/Id/ods-site-code"         "2.16.840.1.113883.2.1.3.2.4.18.48"
+                  })
 
 (defn get-by-oid [root id]
-  (if-let [org (:org (first (jdbc/execute! ds ["SELECT data::varchar as org FROM organisations WHERE id = ?"
-                                               (str root "|" id)])))]
+  (if-let [org (:org (jdbc/execute-one! ds ["SELECT data::varchar as org FROM organisations WHERE id = ?"
+                                            (str root "|" id)]))]
     {:status  200
      :headers {"Content-Type" "application/json"}
      :body    org}
     {:status 404}))
 
+(defn get-postcode [pcode]
+  (if-let [pc (:pc (jdbc/execute-one! ds ["SELECT data::varchar as pc FROM postcodes where pcd2 = ?"
+                                          (postcode/egif pcode)]))]
+    {:status  200
+     :headers {"Content-Type" "application/json"}
+     :body    pc}
+    {:status 404}))
 
 (defroutes app-routes
            (GET "/v1/oid/:root/:id" [root id] (get-by-oid root id))
            (GET "/v1/resolve/:id" [id system]
              (get-by-oid (get system->oid system) id))
+           (GET "/v1/postcode/:postcode" [postcode] (get-postcode postcode))
            (GET "/v1/search" []
              {:status 500
               :body   "Not implemented"})
