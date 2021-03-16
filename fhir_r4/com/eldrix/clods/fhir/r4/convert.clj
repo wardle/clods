9d30d203f1c3a53dc0b8d347a7b69a7ef7599bf3
Mark Wardle <mark@wardle.org>
2021-03-16 17:59:40 +0000

    Add support for FHIR organization type.

--- fhir_r4/com/eldrix/clods/fhir/r4/convert.clj
+++ fhir_r4/com/eldrix/clods/fhir/r4/convert.clj
@@ -1,6 +1,7 @@
 (ns com.eldrix.clods.fhir.r4.convert
-  (:require [com.eldrix.clods.core :as clods])
-  (:import (org.hl7.fhir.r4.model Address Identifier Organization ContactPoint ContactPoint$ContactPointSystem StringType Identifier$IdentifierUse Reference ResourceType)
+  (:require [com.eldrix.clods.core :as clods]
+            [clojure.string :as str])
+  (:import (org.hl7.fhir.r4.model Address Identifier Organization ContactPoint ContactPoint$ContactPointSystem StringType Identifier$IdentifierUse Reference ResourceType Coding CodeableConcept)
            (com.eldrix.clods.core ODS)))
 
 (defn ^Address make-address [org]
@@ -16,6 +17,34 @@
   {"http" ContactPoint$ContactPointSystem/URL
    "tel"  ContactPoint$ContactPointSystem/PHONE
    "fax"  ContactPoint$ContactPointSystem/FAX})
+
+(def role->r4
+  "Mapping of ODS 'role' into FHIR organization 'type'"
+  {"RO33"  "govt"                                           ;; county council
+   "RO90"  "govt"                                           ;; executive agency
+   "RO116" "govt"                                           ;; government agency
+   "RO131" "govt"                                           ;; government department site
+   "RO141" "govt"                                           ;; local authority
+   "RO175" "other"                                          ;; prison
+   })
+
+(defn make-coding [{:keys [codeSystem id displayName]}]
+  (doto (Coding.)
+    (.setSystem codeSystem)
+    (.setCode id)
+    (.setDisplay displayName)))
+
+(defn ^CodeableConcept make-organization-type [^ODS ods {:keys [id isPrimary]}]
+  (let [role (clods/get-role ods id)
+        coding (make-coding role)
+        codings (if-not isPrimary
+                  [coding]
+                  (let [role-code (get role->r4 id "prov")]
+                    (conj [coding] (make-coding {:codeSystem  "http://hl7.org/fhir/ValueSet/organization-type"
+                                               :id          role-code
+                                               :displayName (str/capitalize role-code)}))))]
+    (doto (CodeableConcept.)
+      (.setCoding codings))))
 
 (defn ^ContactPoint make-contact-point [{:keys [type value]}]
   (let [sys (get contact-type->r4 type ContactPoint$ContactPointSystem/OTHER)]
@@ -47,26 +76,32 @@
   (doto (Organization.)
     (.setId ^String (get-in org [:orgId :extension]))
     (.setIdentifier (map make-identifier (clods/org-identifiers org)))
-    (.setActive (:active org))
+    (.setActive (or (:active org) false))
     (.setPartOf (when-let [[root ext] (clods/org-part-of org)]
                   (make-reference (clods/fetch-org ods root ext))))
     (.setAlias (make-aliases ods org))
     (.setAddress [(make-address org)])
     (.setTelecom (map make-contact-point (:contacts org)))
+    (.setType (map (partial make-organization-type ods) (filter :active (:roles org))))
     (.setName (:name org))))
 
 (comment
   (def ods (clods/open-index "/var/tmp/ods" "/var/tmp/nhspd-nov-2020"))
 
   ;; this converts all known organisations into FHIR R4...
-  (do (doall (map make-organization (clods/all-organizations ods)))
+  (do (doall (map (partial make-organization ods) (clods/all-organizations ods)))
       (println "done"))
+
+
   (def org (clods/fetch-org ods nil "X26"))
   (def org (clods/fetch-org ods nil "7A4"))
   org
   (clods/org-identifiers org)
   (map make-identifier (clods/org-identifiers org))
+
   (clods/get-role ods "RO72")
+  (clods/get-role ods "RO116")
+
   (clods/get-relationship ods "RE11")
   (clods/code-systems ods)
   (clods/org-part-of org)
