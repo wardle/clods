d7ecc825891ef5d42342f4fcd5906191ac1bc267
Mark Wardle <mark@wardle.org>
2021-04-30 21:05:24 +0100

    Use namespaced key for environment; add relationships.

--- src/com/eldrix/clods/graph.clj
+++ src/com/eldrix/clods/graph.clj
@@ -7,14 +7,15 @@
             [com.wsscode.pathom3.connect.built-in.resolvers :as pbir]
             [com.wsscode.pathom3.connect.runner :as pcr]
             [com.wsscode.pathom3.interface.eql :as p.eql]
-            [com.wsscode.pathom.viz.ws-connector.core :as pvc]
-            [com.wsscode.pathom.viz.ws-connector.pathom3 :as p.connector]
             [clojure.string :as str]))
+
+(defn target->identifier [{:keys [root extension]}]
+  (hash-map (keyword (str "urn.oid." root) "id") extension))
 
 (pco/defresolver uk-org
   "Resolve a UK organisation using the UK ODS.
   Returns data in Organisation Reference Data (ORD) standard."
-  [{:keys [clods]} {:urn.oid.2.16.840.1.113883.2.1.3.2.4.18.48/keys [id]}]
+  [{::keys [svc]} {:urn.oid.2.16.840.1.113883.2.1.3.2.4.18.48/keys [id]}]
   {::pco/output [:urn.oid.2.16.840.1.113883.2.1.3.2.4.18.48/id
                  :uk.nhs.ord/name :uk.nhs.ord/active
                  {:uk.nhs.ord/orgId [:uk.nhs.ord/root
@@ -29,14 +30,20 @@
                  :uk.nhs.ord.location/postcode :uk.nhs.ord.location/country :uk.nhs.ord.location/uprn
                  :uk.nhs.ord.location/latlon
                  {:uk.nhs.ord/successors [:urn.oid.2.16.840.1.113883.2.1.3.2.4.18.48/id]}
-                 {:uk.nhs.ord/predecessors [:urn.oid.2.16.840.1.113883.2.1.3.2.4.18.48/id]}]}
-  (when-let [org (clods/fetch-org clods nil id)]
+                 {:uk.nhs.ord/predecessors [:urn.oid.2.16.840.1.113883.2.1.3.2.4.18.48/id]}
+                 {:uk.nhs.ord/relationships [:uk.nhs.ord.relationship/id
+                                             :uk.nhs.ord.relationship/active
+                                             :uk.nhs.ord.relationship/startDate
+                                             :uk.nhs.ord.relationship/endDate
+                                             :uk.nhs.ord.relationship/target
+                                             :urn.oid.2.16.840.1.113883.2.1.3.2.4.18.48/id]}]}
+  (when-let [org (clods/fetch-org svc nil id)]
     {:urn.oid.2.16.840.1.113883.2.1.3.2.4.18.48/id id
      :uk.nhs.ord/name                              (:name org)
      :uk.nhs.ord/active                            (:active org)
-     :uk.nhs.ord/orgId                             {:uk.nhs.ord/root                    (get-in org [:orgId :root])
-                                                    :uk.nhs.ord/extension               (get-in org [:orgId :extension])
-                                                    :uk.nhs.ord./assigningAuthorityName (get-in org [:orgId :assigningAuthorityName])}
+     :uk.nhs.ord/orgId                             {:uk.nhs.ord/root                   (get-in org [:orgId :root])
+                                                    :uk.nhs.ord/extension              (get-in org [:orgId :extension])
+                                                    :uk.nhs.ord/assigningAuthorityName (get-in org [:orgId :assigningAuthorityName])}
      :uk.nhs.ord.operational/start                 (get-in org [:operational :start])
      :uk.nhs.ord.operational/end                   (get-in org [:operational :end])
      :uk.nhs.ord/orgRecordClass                    (:orgRecordClass org)
@@ -58,8 +65,19 @@
                                                            :uk.nhs.ord.role/startDate (:startDate role)
                                                            :uk.nhs.ord.role/endDate   (:endDate role)})
                                                         (:roles org))
-     :uk.nhs.ord/successors                        (map (fn [succ] (hash-map (keyword (str "urn.oid." (get-in succ [:target :root])) "id") (get-in succ [:target :extension]))) (:successors org))
-     :uk.nhs.ord/predecessors                      (map (fn [pred] (hash-map (keyword (str "urn.oid." (get-in pred [:target :root])) "id") (get-in pred [:target :extension]))) (:predecessors org))}))
+     :uk.nhs.ord/relationships                     (map (fn [rel]
+                                                          (merge
+                                                            {:uk.nhs.ord.relationship/id        (:id rel)
+                                                             :uk.nhs.ord.relationship/startDate (:startDate rel)
+                                                             :uk.nhs.ord.relationship/endDate   (:endDate rel)
+                                                             :uk.nhs.ord.relationship/active    (:active rel)}
+                                                            (target->identifier (:target rel)))) (:relationships org))
+     :uk.nhs.ord/successors                        (->> (:successors org)
+                                                        (map :target)
+                                                        (map target->identifier))
+     :uk.nhs.ord/predecessors                      (->> (:predecessors org)
+                                                        (map :target)
+                                                        (map target->identifier))}))
 
 
 (pco/defresolver skos-preflabel
@@ -67,7 +85,7 @@
   {:org.w3.2004.02.skos.core/prefLabel name})
 
 (pco/defresolver nhspd-pcds
-  [{:keys [clods]} {:uk.gov.ons.nhspd/keys [PCDS]}]
+  [{::keys [svc]} {:uk.gov.ons.nhspd/keys [PCDS]}]
   {::pco/output [:uk.gov.ons.nhspd/PCDS
                  :uk.gov.ons.nhspd/PCD2
                  :uk.gov.ons.nhspd/LSOA01
@@ -75,7 +93,7 @@
                  :uk.gov.ons.nhspd/PCT
                  :uk.gov.ons.nhspd/OSNRTH1M
                  :uk.gov.ons.nhspd/OSEAST1M]}
-  (when-let [pc (clods/fetch-postcode clods PCDS)]
+  (when-let [pc (clods/fetch-postcode svc PCDS)]
     {:uk.gov.ons.nhspd/PCDS     (get pc "PCDS")
      :uk.gov.ons.nhspd/PCD2     (get pc "PCD2")
      :uk.gov.ons.nhspd/LSOA01   (get pc "LSOA01")
@@ -85,20 +103,28 @@
      :uk.gov.ons.nhspd/OSEAST1M (get pc "OSEAST1M")}))
 
 (pco/defresolver org-primary-role-type-resolver
-  [{:keys [clods]} {:uk.nhs.ord.primaryRole/keys [id]}]
+  [{::keys [svc]} {:uk.nhs.ord.primaryRole/keys [id]}]
   {::pco/output [:uk.nhs.ord.primaryRole/displayName
                  :uk.nhs.ord.primaryRole/codeSystem]}
-  (when-let [result (get (clods/code-systems clods) ["2.16.840.1.113883.2.1.3.2.4.17.507" id])]
+  (when-let [result (get (clods/code-systems svc) ["2.16.840.1.113883.2.1.3.2.4.17.507" id])]
     {:uk.nhs.ord.primaryRole/displayName (:displayName result)
      :uk.nhs.ord.primaryRole/codeSystem  (:codeSystem result)}))
 
 (pco/defresolver org-role-type-resolver
-  [{:keys [clods]} {:uk.nhs.ord.role/keys [id]}]
+  [{::keys [svc]} {:uk.nhs.ord.role/keys [id]}]
   {::pco/output [:uk.nhs.ord.role/displayName
                  :uk.nhs.ord.role/codeSystem]}
-  (when-let [result (get (clods/code-systems clods) ["2.16.840.1.113883.2.1.3.2.4.17.507" id])]
+  (when-let [result (get (clods/code-systems svc) ["2.16.840.1.113883.2.1.3.2.4.17.507" id])]
     {:uk.nhs.ord.role/displayName (:displayName result)
      :uk.nhs.ord.role/codeSystem  (:codeSystem result)}))
+
+(pco/defresolver org-rel-type-resolver
+  [{::keys [svc]} {:uk.nhs.ord.relationship/keys [id]}]
+  {::pco/output [:uk.nhs.ord.relationship/displayName
+                 :uk.nhs.ord.relationship/codeSystem]}
+  (when-let [result (get (clods/code-systems svc) ["2.16.840.1.113883.2.1.3.2.4.17.508" id])]
+    {:uk.nhs.ord.relationship/displayName (:displayName result)
+     :uk.nhs.ord.relationship/codeSystem  (:codeSystem result)}))
 
 (pco/defresolver wgs36
   [{:uk.gov.ons.nhspd/keys [OSEAST1M OSNRTH1M]}]
@@ -107,27 +133,49 @@
   (com.eldrix.nhspd.coords/osgb36->wgs84 OSEAST1M OSNRTH1M))
 
 (def all-resolvers
+  "UK ODS resolvers; expect a key :com.eldrix.clods.graph/svc in environment."
   [uk-org
+   (pbir/alias-resolver :uk.nhs.ord.location/postcode :uk.gov.ons.nhspd/PCDS)
    (pbir/alias-resolver :uk.gov.ons.nhspd/PCT :urn.oid.2.16.840.1.113883.2.1.3.2.4.18.48/id)
    nhspd-pcds
    org-primary-role-type-resolver
    org-role-type-resolver
+   org-rel-type-resolver
    wgs36
    skos-preflabel])
 
+
+(def role->snomed
+  {"RO7"   [284546000 980751000000109]
+   "RO76"  [264358009 1101131000000105]
+   "RO177" [264358009 1101131000000105]})
+
 (comment
-  (def clods (clods/open-index "/var/tmp/ods" "/var/tmp/nhspd"))
-  (def org (clods/fetch-org clods nil "RWM"))
+  (def clods (clods/open-index "/var/tmp/ods-2021-04-28" "/var/tmp/nhspd"))
+  (def org (clods/fetch-org clods nil "7A3B7"))
   (map (fn [succ] (hash-map (keyword (str "urn:oid:" (get-in succ [:target :root])) "id") (get-in succ [:target :extension]))) (:predecessors org))
   (get (clods/code-systems clods) ["2.16.840.1.113883.2.1.3.2.4.17.507" "RO148"])
   (get (clods/fetch-postcode clods "cf14 4xw") "LSOA11")
   (clods/code-systems clods)
   22232009
   (def registry (-> (pci/register all-resolvers)
-                    (assoc :clods clods)))
+                    (assoc ::svc clods)))
+  (require '[com.wsscode.pathom.viz.ws-connector.core :as pvc])
+  (require '[com.wsscode.pathom.viz.ws-connector.pathom3 :as p.connector])
+
   (p.connector/connect-env registry {::pvc/parser-id 'clods})
 
   (uk-org {:clods clods} {:urn:oid:2.16.840.1.113883.2.1.3.2.4.18.48/id "RWM"})
   :urn.ogc.def.crs.EPSG.4326/latitude
   :urn.ogc.def.crs.EPSG.4326/longitude
+
+  (p.eql/process registry
+                 [{[:uk.gov.ons.nhspd/PCDS "cf14 4xw"]
+                   [:uk.gov.ons.nhspd/LSOA11
+                    :uk.gov.ons.nhspd/OSNRTH1M
+                    :uk.gov.ons.nhspd/OSEAST1M
+                    :uk.gov.ons.nhspd/PCT
+                    :uk.nhs.ord/name
+                    :uk.nhs.ord.primaryRole/displayName
+                    {:uk.nhs.ord/predecessors [:uk.nhs.ord/name]}]}])
   )
