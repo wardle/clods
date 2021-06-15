8268fd2081f352296d50a022eba560b8e91f4065
Mark Wardle <mark@wardle.org>
2021-06-15 11:35:28 +0100

    Fix urn:oid: prefix. Add start of FHIR r4 resolution to graph.

--- src/com/eldrix/clods/graph.clj
+++ src/com/eldrix/clods/graph.clj
@@ -15,8 +15,8 @@
 (pco/defresolver uk-org
   "Resolve a UK organisation using the UK ODS.
   Returns data in Organisation Reference Data (ORD) standard."
-  [{::keys [svc]} {:urn.oid.2.16.840.1.113883.2.1.3.2.4.18.48/keys [id]}]
-  {::pco/output [:urn.oid.2.16.840.1.113883.2.1.3.2.4.18.48/id
+  [{::keys [svc]} {:urn:oid:2.16.840.1.113883.2.1.3.2.4.18.48/keys [id]}]
+  {::pco/output [:urn:oid:2.16.840.1.113883.2.1.3.2.4.18.48/id
                  :uk.nhs.ord/name :uk.nhs.ord/active
                  {:uk.nhs.ord/orgId [:uk.nhs.ord/root
                                      :uk.nhs.ord/extension
@@ -29,16 +29,16 @@
                  :uk.nhs.ord.location/address1 :uk.nhs.ord.location/address2 :uk.nhs.ord.location/town
                  :uk.nhs.ord.location/postcode :uk.nhs.ord.location/country :uk.nhs.ord.location/uprn
                  :uk.nhs.ord.location/latlon
-                 {:uk.nhs.ord/successors [:urn.oid.2.16.840.1.113883.2.1.3.2.4.18.48/id]}
-                 {:uk.nhs.ord/predecessors [:urn.oid.2.16.840.1.113883.2.1.3.2.4.18.48/id]}
+                 {:uk.nhs.ord/successors [:urn:oid:2.16.840.1.113883.2.1.3.2.4.18.48/id]}
+                 {:uk.nhs.ord/predecessors [:urn:oid:2.16.840.1.113883.2.1.3.2.4.18.48/id]}
                  {:uk.nhs.ord/relationships [:uk.nhs.ord.relationship/id
                                              :uk.nhs.ord.relationship/active
                                              :uk.nhs.ord.relationship/startDate
                                              :uk.nhs.ord.relationship/endDate
                                              :uk.nhs.ord.relationship/target
-                                             :urn.oid.2.16.840.1.113883.2.1.3.2.4.18.48/id]}]}
+                                             :urn:oid:2.16.840.1.113883.2.1.3.2.4.18.48/id]}]}
   (when-let [org (clods/fetch-org svc nil id)]
-    {:urn.oid.2.16.840.1.113883.2.1.3.2.4.18.48/id id
+    {:urn:oid:2.16.840.1.113883.2.1.3.2.4.18.48/id id
      :uk.nhs.ord/name                              (:name org)
      :uk.nhs.ord/active                            (:active org)
      :uk.nhs.ord/orgId                             {:uk.nhs.ord/root                   (get-in org [:orgId :root])
@@ -80,6 +80,24 @@
                                                         (map target->identifier))}))
 
 
+(def orgRecordClass->namespace {:RC1 "https://fhir.nhs.uk/Id/ods-organization"
+                                :RC2 "https://fhir.nhs.uk/Id/ods-site"})
+
+(pco/defresolver uk-org->fhir-org-identifiers
+  [{:uk.nhs.ord/keys [orgId orgRecordClass]}]
+  {::pco/output [{:org.hl7.fhir.Organization/identifier [:org.hl7.fhir.Identifier/system
+                                                         :org.hl7.fhir.Identifier/value]}]}
+  {:org.hl7.fhir.Organization/identifier [{:org.hl7.fhir.Identifier/system (:uk.nhs.ord/root orgId)
+                                           :org.hl7.fhir.Identifier/value  (:uk.nhs.ord/extension orgId)}
+                                          {:org.hl7.fhir.Identifier/system (keyword (str "urn:oid." (:uk.nhs.ord/root orgId)))
+                                           :org.hl7.fhir.Identifier/value (:uk.nhs.ord/extension orgId)}
+                                          {:org.hl7.fhir.Identifier/system (get orgRecordClass->namespace orgRecordClass)
+                                           :org.hl7.fhir.Identifier/value (:uk.nhs.ord/extension orgId)}]})
+
+(pco/defresolver uk-org->fhir-org-name
+  [{:uk.nhs.ord/keys [name]}]
+  {:org.hl7.fhir.Organization/name name})
+
 (pco/defresolver skos-preflabel
   [{:uk.nhs.ord/keys [name]}]
   {:org.w3.2004.02.skos.core/prefLabel name})
@@ -105,7 +123,7 @@
 (pco/defresolver nhspd-pct-org
   [{:uk.gov.ons.nhspd/keys [PCT]}]
   {::pco/output [{:uk.gov.ons.nhspd/PCT_ORG [:urn:oid:2.16.840.1.113883.2.1.3.2.4.18.48/id]}]}
-  {:uk.gov.ons.nhspd/PCT_ORG {:urn.oid.2.16.840.1.113883.2.1.3.2.4.18.48/id PCT}})
+  {:uk.gov.ons.nhspd/PCT_ORG {:urn:oid:2.16.840.1.113883.2.1.3.2.4.18.48/id PCT}})
 
 (pco/defresolver org-primary-role-type-resolver
   [{::keys [svc]} {:uk.nhs.ord.primaryRole/keys [id]}]
@@ -137,6 +155,14 @@
                  :urn.ogc.def.crs.EPSG.4326/longitude]}
   (com.eldrix.nhspd.coords/osgb36->wgs84 OSEAST1M OSNRTH1M))
 
+(pco/defmutation search
+  "Performs a search. Parameters:
+    |- :s                  : search string to use"
+  [{::keys [svc]} params]
+  {::pco/op-name ':uk.nhs.ord/search
+   ::pco/params  [:s]
+   ::pco/output  []})
+
 (def all-resolvers
   "UK ODS resolvers; expect a key :com.eldrix.clods.graph/svc in environment."
   [uk-org
@@ -147,7 +173,10 @@
    org-role-type-resolver
    org-rel-type-resolver
    wgs36
-   skos-preflabel])
+   skos-preflabel
+   uk-org->fhir-org-identifiers
+   uk-org->fhir-org-name
+   search])
 
 
 (def role->snomed
@@ -170,10 +199,19 @@
 
   (p.connector/connect-env registry {:com.wsscode.pathom.viz.ws-connector.core/parser-id 'clods})
 
-  (uk-org {:clods clods} {:urn:oid:2.16.840.1.113883.2.1.3.2.4.18.48/id "RWM"})
+  (uk-org {::svc clods}
+          {:urn:oid:2.16.840.1.113883.2.1.3.2.4.18.48/id "RWM"})
+
   :urn.ogc.def.crs.EPSG.4326/latitude
   :urn.ogc.def.crs.EPSG.4326/longitude
 
+  (p.eql/process registry
+                 [{[:urn:oid:2.16.840.1.113883.2.1.3.2.4.18.48/id "7A4"]
+                   [:urn:oid:2.16.840.1.113883.2.1.3.2.4.18.48/id
+                    :uk.nhs.ord/name
+                    :uk.nhs.ord/orgId
+                    :org.hl7.fhir.Organization/identifier
+                    :org.hl7.fhir.Organization/name]}])
   (p.eql/process registry
                  [{[:uk.gov.ons.nhspd/PCDS "CF14 4XW"]
                    [:uk.gov.ons.nhspd/LSOA11
@@ -186,4 +224,14 @@
                                                 :uk.nhs.ord/name
                                                 :uk.nhs.ord.primaryRole/displayName
                                                 {:uk.nhs.ord/predecessors [:uk.nhs.ord/name]}]}]}])
+  (clods/search-org clods {:n "Uni Wales" :roles "RO148"})
+
+  (p.eql/process
+    registry
+    [{[:urn:oid:2.16.840.1.113883.2.1.3.2.4.18.48/id "7A4BV"]
+      [:org.w3.2004.02.skos.core/prefLabel
+       :uk.nhs.ord/name]}
+     {[:urn:oid:2.16.840.1.113883.2.1.3.2.4.18.48/id "RWM"]
+      [:org.w3.2004.02.skos.core/prefLabel
+       :uk.nhs.ord/name]}])
   )
